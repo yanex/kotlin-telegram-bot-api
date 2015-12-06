9a5ec1aba48e3d186e57227eeccbc98a94614ffc
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-12-06 04:22:13 +0300

    Result value made nullable

--- bot-api/src/main/java/org/yanex/telegram/TelegramBotService.kt
+++ bot-api/src/main/java/org/yanex/telegram/TelegramBotService.kt
@@ -8,7 +8,7 @@ import java.nio.file.Files
 import com.google.gson.annotations.SerializedName as Name
 
 data class Response<T>(
-        val result: T,
+        val result: T?,
         val ok: Boolean,
         @Name("error_code") val errorCode: Int?,
         @Name("description") val errorDescription: String?)
