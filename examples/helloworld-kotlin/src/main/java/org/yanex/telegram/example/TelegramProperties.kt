f26e1431a5d1a5917264f770d320124a340c4960
Suellen Leite <suellenpll@gmail.com>
2017-01-12 03:01:20 -0200

    improving explanation of the FileNotFoundException + let the FILE_NAME variable

--- examples/helloworld-kotlin/src/main/java/org/yanex/telegram/example/TelegramProperties.kt
+++ examples/helloworld-kotlin/src/main/java/org/yanex/telegram/example/TelegramProperties.kt
@@ -1,12 +1,14 @@
 package org.yanex.telegram.example
 
+import java.io.File
 import java.io.FileInputStream
+import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.util.Properties
 
-class TelegramProperties {
+class TelegramProperties(val fileName: String = "telegram.properties") {
     val token: String
-        get() = readProperties().getProperty(PROP_TOKEN) ?: throw RuntimeException("Token is not set in " + FILE_NAME)
+        get() = readProperties().getProperty(PROP_TOKEN) ?: throw RuntimeException("Token is not set in " + fileName)
 
     var lastId: Long
         get() = readProperties().getProperty(PROP_LAST_ID)?.let { it.toLong() } ?: 0
@@ -15,16 +17,18 @@ class TelegramProperties {
         })
 
     private fun writeProperties(props: Properties) {
-        FileOutputStream(FILE_NAME).use { output -> props.store(output, null) }
+        FileOutputStream(fileName).use { output -> props.store(output, null) }
     }
 
     private fun readProperties() = Properties().apply {
-        FileInputStream(FILE_NAME).use { input -> load(input) }
+        try {
+            FileInputStream(fileName).use { input -> load(input) }
+        } catch (e: FileNotFoundException) {
+            throw FileNotFoundException("File ${System.getProperty("user.dir")}${File.separator}$fileName not found.")
+        }
     }
 
-    private companion object {
-        val FILE_NAME = "telegram.properties"
-
+    companion object {
         val PROP_TOKEN = "token"
         val PROP_LAST_ID = "lastId"
     }
